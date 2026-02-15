4b52b4269b853f1af6de8fb7d4a949f43a646c6b
diigbd <diigbd@gmail.com>
2026-02-15 16:01:43 -0500

    Update Education.tsx

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -18,11 +18,10 @@ const Clients = () => {
         <section className="bg-primary py-24">
           <div className="container mx-auto px-6">
             <h1 className="text-4xl md:text-5xl font-display font-bold text-primary-foreground mb-4">
-              Consulting
+              Education
             </h1>
             <p className="text-xl text-primary-foreground/80 max-w-2xl">
-              We provide consulting services for socially-minded businesses of all sizes. Our teams have worked on cases ranging from market research to competitive analyses to customer discovery. We equip our clients with capable and trained undergraduate consulting teams to help businesses grow and scale. We hope to drive social impact and value for our clients and our investments. We aspire to set up businesses with capable and trained undergraduate consulting teams to help businesses grow and scale, with a major goal of generating significant positive social impact.
-            </p>
+              The education division aims to promote knowledge of and interest in impact investing within the Duke community. Each semester, we teach a half-credit course designed as an introduction to the impact investing industry as well as organize DIIG's Impact Fellowship program for new analysts. We instruct students on best methods of quantifying financial and social impact, guide them through case studies, and invite guest speakers from the industry.            </p>
           </div>
         </section>
 
