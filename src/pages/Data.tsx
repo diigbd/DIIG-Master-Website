ed6b13e38fb33da6241f0e24dd8dbd1ffb47f292
diigbd <diigbd@gmail.com>
2026-02-15 15:52:51 -0500

    Update Data.tsx

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -18,11 +18,10 @@ const Clients = () => {
         <section className="bg-primary py-24">
           <div className="container mx-auto px-6">
             <h1 className="text-4xl md:text-5xl font-display font-bold text-primary-foreground mb-4">
-              Consulting
+              Data
             </h1>
             <p className="text-xl text-primary-foreground/80 max-w-2xl">
-              We provide consulting services for socially-minded businesses of all sizes. Our teams have worked on cases ranging from market research to competitive analyses to customer discovery. We equip our clients with capable and trained undergraduate consulting teams to help businesses grow and scale. We hope to drive social impact and value for our clients and our investments. We aspire to set up businesses with capable and trained undergraduate consulting teams to help businesses grow and scale, with a major goal of generating significant positive social impact.
-            </p>
+              The Data team empowers clients to achieve their maximum potential by providing data-driven solutions to their business questions. Our team comprises of data analysts from diverse backgrounds who are equipped to tackle real-life data challenges and drive results.            </p>
           </div>
         </section>
 
