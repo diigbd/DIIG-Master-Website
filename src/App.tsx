88869cf95fe562c56eb1e97b5a8a47e8a9a0978b
Ian Le <ianlllym@gmail.com>
2026-02-20 20:31:45 -0500

    Reset scroll to top of page on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Data from "./pages/divisions/Data";
 import Education from "./pages/divisions/Education";
 import BD from "./pages/divisions/BD";
 import NotFound from "./pages/NotFound";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -23,6 +24,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/divisions" element={<Divisions />} />
